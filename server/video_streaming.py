75d4c18673faf5ae64a8892218eb89ad2c6adcec
johnsengendo <73909628+johnsengendo@users.noreply.github.com>
2025-02-06 22:40:16 +0100

    Update video_streaming.py

--- server/video_streaming.py
+++ server/video_streaming.py
@@ -5,15 +5,15 @@ import subprocess
 
 def main():
     """
-    Main function to handle video streaming without packet capture.
+    Main function to handle video streaming.
     """
-    input_file = "video/Deadpool.mp4"
-    loops_number = -1  # Streaming the video more times
+    input_file = "video/Jam.mp4"
+    loops_number = -1  # Streaming the video multiple times
 
     ffmpeg_command = [
         "ffmpeg", "-loglevel", "info", "-stats", "-re", "-stream_loop", str(loops_number),
         "-i", input_file,
-        "-t", "600",              # Set the streaming duration (in seconds)
+        "-t", "600",              # Setting the streaming duration (in seconds)
         "-c:v", "copy",           # Coping video stream without re-encoding
         "-c:a", "aac",            # Encode audio using AAC
         "-ar", "44100",           # Audio sample rate
